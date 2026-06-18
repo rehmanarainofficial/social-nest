3116df1a37415112ae9c6618bcfd4c8e99b04bd0
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-06-18 10:08:09 +0500

    feat: add Cloudinary service and downgrade NestJS dependencies for compatibility

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
-
 @Injectable()
 export class CloudinaryService {
-  constructor() {
+  constructor(private configService: ConfigService) {
     cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: this.configService.get('CLOUDINARY_CLOUD_NAME'),
+      api_key: this.configService.get('CLOUDINARY_API_KEY'),
+      api_secret: this.configService.get('CLOUDINARY_API_SECRET'),
     });
   }
 
